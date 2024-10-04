171e2e50bc67241f78e295b57b8e65db5dfcb7ac
Anasmit Mitra <anasmitmitra9@gmail.com>
2024-10-04 21:37:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default {
   build: {
     rollupOptions: {
-      external: ['react-router-dom']
+      external: ['react-router-dom', 'bootstrap']
     }
   }
 }
